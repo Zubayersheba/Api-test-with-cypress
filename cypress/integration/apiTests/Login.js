3fd488bd9a7deedc7ba0f1cbfe83494288f80e4d
zubayer alam <zubayer@sheba.xyz>
2022-05-18 14:14:16 +0600

    project up and running

---
+++ cypress/integration/apiTests/Login.js
@@ -0,0 +1,38 @@
+
+const { it } = require("mocha")
+//<referece types ="Cypress" />
+
+let testMobile = "01755883515"
+let testPassword = "14725"
+let jwtToken
+
+describe('Login Test cases',() => {  
+
+   it('Login successful',() => {
+       
+       cy.request({
+          
+           method : 'POST',
+           url : 'https://accounts.dev-sheba.xyz/api/v3/profile/login',
+           //headers : {}
+           body : {
+               mobile : testMobile,
+               password: testPassword
+           }
+           
+       }).then((res) => {
+           //cy.log(JSON.stringify(res.body.token));
+           expect(res.status).to.eq(200);
+           expect(res.body.code).to.eq(200);
+           expect(res.body.message).to.eq("Successful");
+           jwtToken = res.body.token 
+           cy.log(jwtToken);
+
+       })
+
+   });
+
+})
+
+
+
